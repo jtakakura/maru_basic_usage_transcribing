6730a9065bbc792f76d4eeb0390d6dfed6559db6
Junji Takakura <takakura@rightcode.co.jp>
2018-01-23 15:12:40 +0900

    implemented xandra operations

--- lib/maru_app.ex
+++ lib/maru_app.ex
@@ -1,7 +1,20 @@
 defmodule MaruApp.Router.Homepage do
+  require Logger
   use Maru.Router
 
   get do
+    Logger.info "called: get"
+    {:ok, xconn} = Xandra.start_link(nodes: ["127.0.0.1:9042"])
+
+    # query = "INSERT INTO kopt.users (id, name, email) VALUES (uuid(), 'Junji Takakura', 'j.takakura@gmail.com')"
+    # {:ok, %Xandra.Void{}} = Xandra.execute(xconn, query, _params = [])
+
+    # with {:ok, prepared} <- Xandra.prepare(xconn, "SELECT * FROM kopt.users WHERE email = ?"),
+    #   {:ok, %Xandra.Page{} = page} <- Xandra.execute(xconn, prepared, [_email = "j.takakura@gmail.com"]),
+    #   do: Logger.info Enum.to_list(page)
+    user = xconn |> Xandra.execute!("SELECT * FROM kopt.users") |> Enum.fetch!(0)
+    IO.inspect(user)
+
     json(conn, %{hell: :world})
   end
 end
